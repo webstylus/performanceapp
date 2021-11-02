178e943042b08f8afe7b98cb088132f613fed395
Rafael Pacífico <rafael@webstylus.com.br>
2021-11-02 18:49:26 -0300

    First Commit

---
+++ src/components/Friend.tsx
@@ -0,0 +1,24 @@
+import React, {memo} from 'react';
+import {Text} from 'react-native';
+
+interface Props {
+    data: {
+        id: number
+        name: string
+        likes: number
+    }
+}
+
+function FriendComponent({data}: Props) {
+
+    return (
+        <Text>
+            {data.name} - likes: {data.likes}
+        </Text>
+    );
+
+}
+
+export const Friend = memo(FriendComponent, (prevProps, nextProps) => {
+    return Object.is(prevProps.data, nextProps.data)
+})
---
+++ src/components/FriendList.tsx
@@ -0,0 +1,32 @@
+import React, {useMemo} from 'react';
+import {Text, View} from 'react-native';
+import {Friend} from "./Friend";
+
+interface Props {
+    data: {
+        id: number
+        name: string
+        likes: number
+    }[]
+}
+
+export function FriendList({data}: Props) {
+
+    const totalLikes = useMemo(() => {
+        return data.reduce((likes, friend) => {
+            return likes + friend.likes
+        }, 0)
+    }, [data])
+
+    return (
+        <View>
+            <Text>Total de likes: {totalLikes}</Text>
+            {
+                data.map(friend => (
+                    <Friend key={String(friend.id)} data={friend}/>
+                ))
+            }
+        </View>
+    );
+
+}
